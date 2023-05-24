9a704fed0101bf5fc8a9d1f5eebb047a30d03f4f
Dan97O <dan97-oddo@live.it>
2023-05-24 16:09:11 +0200

    add all info cards

--- resources/views/movies.blade.php
+++ resources/views/movies.blade.php
@@ -3,14 +3,27 @@
 @section('content')
     <h1 class="text-center my-4">Movies</h1>
     <div class="container">
-        <div class="row">
+        <div class="row row-cols-1 row-cols-lg-5 g-3">
             @foreach ($movies as $movie)
                 <div class="col">
                     <div class="card">
-                        <div class=" card-text">
-                            {{ $movie->title }}
+                        <div class="card-body">
+                            <h5 class="card-title">{{ $movie->title }}</h5>
+                            <p class="card-text">
+                                <strong>Titolo originale:</strong> {{ $movie->original_title }}
+                            </p>
+                            <p class="card-text">
+                                <strong>Nazionalità:</strong> {{ $movie->nationality }}
+                            </p>
+                            <p class="card-text">
+                                <strong>Data:</strong> {{ $movie->date }}
+                            </p>
+                            <p class="card-text">
+                                <strong>Voto:</strong> {{ $movie->vote }}
+                            </p>
                         </div>
                     </div>
+
                 </div>
             @endforeach
 
